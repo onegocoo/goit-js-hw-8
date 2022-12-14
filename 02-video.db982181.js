b5456b9f66c7f83c621259246e877998bdc81768
onegocoo <101978533+onegocoo@users.noreply.github.com>
2022-12-14 19:39:34 +0000

    Deploying to gh-pages from @ onegocoo/goit-js-hw-8@46e5ce229fe954c0bdbe4c6017c89a134cf1cdb4 🚀

---
+++ 02-video.db982181.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe");console.log(e)}();
+//# sourceMappingURL=02-video.db982181.js.map
---
+++ 02-video.e60eb1ab.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe");console.log(e);
+//# sourceMappingURL=02-video.e60eb1ab.js.map
